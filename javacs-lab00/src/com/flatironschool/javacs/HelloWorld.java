5b64717b425c1a4bef3afc8556ecd49c0747f677
Cassie Turberville <cassie97@vt.edu>
2016-05-17 21:04:42 -0400

    first commit

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -1,11 +1,13 @@
 package com.flatironschool.javacs;
 
 public class HelloWorld {
-    public static Double getVersion() {
-        // I won't work!
+    public static Double getVersion() 
+    {
+        return Double.parseDouble(System.getProperty("java.version"));
     }
 
-    public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+    public static void main(String[] args) 
+    {
+	  System.out.println (getVersion());
     }
 }
